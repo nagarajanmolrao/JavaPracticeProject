29cf214237abdd2f33bde2ba0b3d9cf9ca137fa5
n-anmol <n.anmol@capgemini.com>
2023-08-10 11:27:36 +0530

    Armstrong numbers solution streams implementation

--- src/main/java/com/nanmolrao/armstrong_number/Solution.java
+++ src/main/java/com/nanmolrao/armstrong_number/Solution.java
@@ -1,7 +1,10 @@
 package com.nanmolrao.armstrong_number;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Print all Armstrong number between 1 and 1000.
@@ -18,10 +21,10 @@ public class Solution {
      *
      * @return the string of armstrong numbers between 1 and 1000
      */
-    public static String getArmstrongNumbers(){
+    public static String getArmstrongNumbers() {
         StringBuilder returnString = new StringBuilder();
-        for(int i = 1;i<=1000;i++){
-            if(checkForArmstrongNumber(i)){
+        for (int i = 1; i <= 1000; i++) {
+            if (checkForArmstrongNumber(i)) {
                 returnString.append(i);
                 returnString.append(" ");
             }
@@ -35,16 +38,36 @@ public class Solution {
      * @param number the number to be checked
      * @return returns true if the number is Armstrong number, else returns false
      */
-    public static boolean checkForArmstrongNumber(Integer number){
+    public static boolean checkForArmstrongNumber(Integer number) {
         int n = String.valueOf(number).length();
         double sum = 0D;
-        for(String eachDigit: String.valueOf(number).split("")){
+        for (String eachDigit : String.valueOf(number).split("")) {
             sum += Math.pow(Double.parseDouble(eachDigit), n);
         }
         return number == (int) sum;
     }
 
+    /**
+     * Get armstrong numbers between 1 and 1000 using streams.
+     *
+     * @return the string of armstrong numbers between 1 and 1000
+     */
+    public static String getArmstrongNumbersV2() {
+        return IntStream.rangeClosed(1, 1000)
+                .filter(number -> {
+                    int n = String.valueOf(number).length();
+                    double sum = Arrays.stream(String.valueOf(number).split(""))
+                            .mapToDouble(Integer::parseInt)
+                            .map(digit -> Math.pow(digit, n))
+                            .sum();
+                    return number == (int) sum;
+                })
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+    }
+
     public static void main(String[] args) {
-        log.log(Level.INFO, ()->"Armstrong numbers between 1 and 1000:\n" + getArmstrongNumbers());
+        log.log(Level.INFO, () -> "Armstrong numbers between 1 and 1000:\n" + getArmstrongNumbers());
+        log.log(Level.INFO, () -> "Armstrong numbers between 1 and 1000(with streams):\n" + getArmstrongNumbersV2());
     }
 }
