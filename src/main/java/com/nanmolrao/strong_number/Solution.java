39c052542ab8c2bd9966e25028e9874dd1c37e6c
n-anmol <n.anmol@capgemini.com>
2023-08-10 10:28:34 +0530

    Strong number check solution

---
+++ src/main/java/com/nanmolrao/strong_number/Solution.java
@@ -0,0 +1,34 @@
+package com.nanmolrao.strong_number;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Check the given number is Strong number or not.
+ * Strong number is a special number whose sum of factorial of digits is equal to the original number.
+ * For example: 145 is strong number. Since, 1! + 4! + 5! = 145
+ **/
+public class Solution {
+
+    public static final Logger log = Logger.getLogger(Solution.class.getName());
+
+    public static boolean checkStrongNumber(Integer number) {
+        int sum = 0;
+        for(String eachDigit: String.valueOf(number).split("")){
+           sum += factorial(Integer.parseInt(eachDigit));
+        }
+        return sum==number;
+    }
+
+    public static Integer factorial(Integer num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        log.log(Level.INFO, () -> "Result: " + checkStrongNumber(145));
+    }
+}
